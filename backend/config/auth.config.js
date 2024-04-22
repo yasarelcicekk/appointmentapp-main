c9d2af0f10b2fc393533fc36800471c1f51a27cd
Kadd <kadirkavraz@gmail.com>
2024-04-22 03:39:51 +0300

    Remove sensitive files from tracking

--- backend/config/auth.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    secret: "e74323e486234523482345234523452345234523452345234523452345234523",
-    email:"kadyas000@gmail.com",
-    pass:"gott hyzm ocdp bwfp"
-  };
--- backend/config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HOST: "0.0.0.0",
-    PORT: 27017,
-    DB: "appointmentsystem",
-    serverURL:'mongodb+srv://kadirorhunkavraz:0kE8a5etArYYBvgb@mernappcluster0.1nf2aoq.mongodb.net/?retryWrites=true&w=majority'
-  };
